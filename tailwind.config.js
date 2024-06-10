8fce0476bee4c37cf89ae8fb459dce6172edc13d
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-10 20:45:25 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     './index.{js,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,ts,tsx}',,
+    './components/**/*.{js,ts,tsx}',
+    ,
   ],
 
   presets: [require('nativewind/preset')],
