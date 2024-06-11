114c68975e51ca1938ba3604107f8e6eaddfe367
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-11 12:08:06 -0300

    feat: add page register

--- src/app/(drawer)/(tabs)/home/index.tsx
+++ src/app/(drawer)/(tabs)/home/index.tsx
@@ -4,17 +4,25 @@ import { Menu } from '~/components/Menu';
 import LastWorkout from '~/components/LastWorkout';
 import { ListRoutines } from '~/components/ListRoutines';
 import { Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function Page() {
+  const router = useRouter();
   return (
     <>
       <Menu />
       <Container>
-        <Button className="mt-10 mb-6" titleButton="Montar meu treino" title="Criar rotina" />
+        <Button
+          className="mb-6 mt-10"
+          titleButton="Montar meu treino"
+          title="Criar rotina"
+          onPress={() => router.push('/register')}
+        />
+
         <LastWorkout titleButton="Perna" title="Último treino" />
 
         <View>
-          <Text className="mt-5 text-lg font-bold color-text-900">Minhas rotinas</Text>
+          <Text className="color-text-900 mt-5 text-lg font-bold">Minhas rotinas</Text>
           <ListRoutines />
           <ListRoutines />
         </View>
