baa2d58994d1fa95f18eac8623675eba8bbe8940
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-10 21:27:39 -0300

    refactor:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.{js,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,ts,tsx}',,
+    './App.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
   ],
-
-  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
