3a65f6c4e4fec945b4c97f7fb96ebeab62e23252
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-17 22:29:00 -0300

    feat: add styles component new and code refactoring

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,18 +5,31 @@ interface ButtonProps extends TouchableOpacityProps {
   title?: string;
   titleButton: string;
   className?: string;
+  touchableStyle?: string;
+  textStyle?: string;
   borderColor?: string;
 }
 
-export default function Button({ onPress, title, titleButton, className, borderColor }: ButtonProps) {
+export default function Button({
+  onPress,
+  title,
+  titleButton,
+  className,
+  borderColor,
+  touchableStyle,
+  textStyle,
+}: ButtonProps) {
   return (
-    <View className={`${className}`}>
-      <Text className="color-text-900 text-xl font-semibold">{title}</Text>
+    <>
+      <Text className="text-xl font-semibold color-text-900">{title}</Text>
       <TouchableOpacity
-        className={`h-14 items-center justify-center rounded-md border-2 ${borderColor ? borderColor : 'border-border-400'}`}
+        className={`h-14 items-center justify-center rounded-md  
+          ${borderColor ? borderColor : 'border-border-400'} ${touchableStyle ? touchableStyle : 'border-2'} ${className}`}
         onPress={onPress}>
-        <Text className="color-text-600 text-lg font-semibold">{titleButton}</Text>
+        <Text className={`text-lg font-semibold  ${textStyle ? textStyle : 'color-text-600'}`}>
+          {titleButton}
+        </Text>
       </TouchableOpacity>
-    </View>
+    </>
   );
 }
