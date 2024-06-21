067b29a9102b73a06cf289e6d4fd87ab1959b497
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-21 12:52:15 -0300

    feat: add button edit routine

--- src/components/ListRoutines/index.tsx
+++ src/components/ListRoutines/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Button from '../Button';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function ListRoutines() {
   return (
     <>
       <View className="mb-3.5 rounded-md border-2 border-border-50 p-4">
         <View>
-          <Text className="text-lg font-bold color-text-900">Perna</Text>
+          <View className="flex flex-row justify-between">
+            <Text className="text-lg font-bold color-text-900">Perna</Text>
+            <TouchableOpacity>
+              <Ionicons name="ellipsis-vertical" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
           <Text className="color-text-600">
             Agachamento, Hack, Leg Press 45, bulgáro, Cadeira extensora...
           </Text>
