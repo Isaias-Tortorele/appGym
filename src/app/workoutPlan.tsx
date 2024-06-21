43eaa5b93f2f04e932f36d274ead19e33a626d23
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-20 21:04:57 -0300

    feat: add page to edit and save workout plan

---
+++ src/app/workoutPlan.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { router } from 'expo-router';
+import Container from '~/components/ui/Container';
+import Button from '~/components/Button';
+import { ScrollView, Text, View } from 'react-native';
+import { Avatar, AvatarImage } from '~/components/ui/Avatar';
+import { useExercise } from '~/contexts/ExerciseContext';
+
+export default function WorkoutPlan() {
+  const { selectedExercises } = useExercise();
+
+  const handleStartWorkout = () => router.push('/home');
+  const handleEditWorkout = () => router.push('/exercises');
+
+  return (
+    <Container>
+      <View className="mt-20">
+        <Text className="mb-4 text-xl font-semibold">Ficha: Perna</Text>
+        <ScrollView className="max-h-[70%] min-h-[70%]">
+          {selectedExercises.map((exercise) => (
+            <View
+              className="mb-4 rounded-3xl border-x-2 border-b-2 border-slate-300 bg-slate-100 px-4 shadow-sm shadow-slate-400"
+              key={exercise.id}>
+              <View className="h-16 flex-row items-center justify-between">
+                <View className="flex flex-row items-center justify-center gap-4">
+                  <Avatar className="h-14 w-14">
+                    <AvatarImage source={{ uri: exercise.url_gif }} resizeMode="cover" />
+                  </Avatar>
+                  <Text className="text-lg font-semibold">{exercise.title}</Text>
+                </View>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+
+        <Button
+          titleButton="Editar treino"
+          className=""
+          touchableStyle="border-2"
+          textStyle="text-text-600"
+          onPress={handleEditWorkout}
+        />
+        <Button
+          titleButton="Salvar treino"
+          className=""
+          touchableStyle="bg-cyan-400 border-none"
+          textStyle="text-white"
+          onPress={handleStartWorkout}
+        />
+      </View>
+    </Container>
+  );
+}
