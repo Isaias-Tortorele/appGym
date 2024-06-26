d347749e81f4bb8232ec6edc41d863cf8511ee98
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-06-26 19:37:33 -0300

    feat: add component Separator

---
+++ src/components/ui/Separator.tsx
@@ -0,0 +1,6 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function Separator() {
+  return <View className="w-4/5 border mx-auto border-border-400 m-5" />;
+}
